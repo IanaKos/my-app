93791b4b3cc4e45b3c0726b0820d272b73beb08e
IanaKos <95440446+IanaKos@users.noreply.github.com>
2023-12-21 22:52:53 +0100

    st

---
+++ my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import "./index.css";
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<React.StrictMode></React.StrictMode>);
+
+// const div = document.createElement("div");
+const root = document.getElementById("root");
+
+// root.append(div);
+
+const div = React.createElement("div");
+ReactDOM.createRoot(root).render(div);
